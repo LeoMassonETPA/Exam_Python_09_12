80e628af4bd3566a1c598d660215a67ae0ba6245
Léo Masson <l.masson@rns.ecolescreatives.com>
2020-12-09 15:21:23 +0100

    Update Exam_Python_Leo_Masson_09_12.py

--- Exam_Python_Leo_Masson_09_12.py
+++ Exam_Python_Leo_Masson_09_12.py
@@ -5,9 +5,17 @@ from colorama import Fore, Back, Style
 import random
 
 liste_mot = ["hideux", "picole", "crayon", "fusain", "paumes", "charge", "tisser", "whisky", "chlore", "poulet"]
+mot_mystere = random.choices(liste_mot)  
 
-mot_mystere = random.choices(liste_mot)
-print (liste_mot)
-print(mot_mystere)
+proposition = input ("Votre proposition ?(6 lettres max)")
 
+    if (proposition[0] == mot_mystere[0]):
+        print (Fore.RED + proposition[0], end=" ")
+    elif (proposition[0] == mot_mystere[1] or proposition[0] == mot_mystere[2] or proposition[0] == mot_mystere[3] or proposition[0] == mot_mystere[4] or proposition[0] == mot_mystere[5]):
+        print (Fore.YELLOW + proposition[0], end=" ")
+    else :
+        print (Fore.BLUE + proposition[0], end=" ")
+        
+   
+    
 input ()
