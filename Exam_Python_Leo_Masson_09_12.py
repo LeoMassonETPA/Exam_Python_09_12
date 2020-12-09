024f1a39d69c3cbfc5dcdf3be73d2726b6bf8939
Léo Masson <l.masson@rns.ecolescreatives.com>
2020-12-09 16:06:17 +0100

    Update Exam_Python_Leo_Masson_09_12.py

--- Exam_Python_Leo_Masson_09_12.py
+++ Exam_Python_Leo_Masson_09_12.py
@@ -4,54 +4,65 @@ from colorama import Fore, Back, Style
 
 import random
 
-liste_mot = ["hideux", "picole", "crayon", "fusain", "paumes", "charge", "tisser", "whisky", "chlore", "poulet"]       #Selection aleatoire du mot
+liste_mot = ["hideux", "picole", "crayon", "fusain", "paumes", "charge", "tisser", "whisky", "chlore", "poulet"]       #Sélection aléatoire du mot
 mot_alea = random.choices(liste_mot)  
-mot_mystere =(mot_alea[0])                                                                                             # Aucune idée de pourquoi ça marche 
+mot_mystere =(mot_alea[0])                                                                                             # <-- Aucune idée de pourquoi cette ligne résoud mon problème ça marche mais ça marche ! 
 
 print (mot_mystere)
 
-proposition = input ("Votre proposition ?(6 lettres max)")
 
-if(proposition[0] == mot_mystere[0]):
-    print (Fore.RED + proposition[0], end=" ")
-elif(proposition[0] == mot_mystere[1] or proposition[0] == mot_mystere[2] or proposition[0] == mot_mystere[3] or proposition[0] == mot_mystere[4] or proposition[0] == mot_mystere[5]):
-    print (Fore.YELLOW + proposition[0], end=" ")
-else:
-    print (Fore.BLUE + proposition[0], end=" ")
+victoire = False
+tour = 0
 
-if(proposition[1] == mot_mystere[1]):
-    print (Fore.RED + proposition[1], end=" ")
-elif(proposition[1] == mot_mystere[0] or proposition[1] == mot_mystere[2] or proposition[1] == mot_mystere[3] or proposition[1] == mot_mystere[4] or proposition[1] == mot_mystere[5]):
-    print (Fore.YELLOW + proposition[1], end=" ")
-else:
-    print (Fore.BLUE + proposition[1], end=" ")
-    
-if(proposition[2] == mot_mystere[2]):
-    print (Fore.RED + proposition[2], end=" ")
-elif(proposition[2] == mot_mystere[0] or proposition[2] == mot_mystere[1] or proposition[2] == mot_mystere[3] or proposition[2] == mot_mystere[4] or proposition[2] == mot_mystere[5]):
-    print (Fore.YELLOW + proposition[2], end=" ")
-else:
-    print (Fore.BLUE + proposition[2], end=" ")
 
-if(proposition[3] == mot_mystere[3]):
-    print (Fore.RED + proposition[3], end=" ")
-elif(proposition[3] == mot_mystere[0] or proposition[3] == mot_mystere[1] or proposition[3] == mot_mystere[2] or proposition[3] == mot_mystere[4] or proposition[3] == mot_mystere[5]):
-    print (Fore.YELLOW + proposition[3], end=" ")
-else:
-    print (Fore.BLUE + proposition[3], end=" ")
+while (victoire = False or tour < 9):
+
+    proposition = input ("Votre proposition ?(6 lettres max)")
+
+    if(proposition[0] == mot_mystere[0]):
+        print (Fore.RED + proposition[0], end=" ")
+    elif(proposition[0] == mot_mystere[1] or proposition[0] == mot_mystere[2] or proposition[0] == mot_mystere[3] or proposition[0] == mot_mystere[4] or proposition[0] == mot_mystere[5]):
+        print (Fore.YELLOW + proposition[0], end=" ")
+    else:
+        print (Fore.BLUE + proposition[0], end=" ")
+
+    if(proposition[1] == mot_mystere[1]):
+        print (Fore.RED + proposition[1], end=" ")
+    elif(proposition[1] == mot_mystere[0] or proposition[1] == mot_mystere[2] or proposition[1] == mot_mystere[3] or proposition[1] == mot_mystere[4] or proposition[1] == mot_mystere[5]):
+        print (Fore.YELLOW + proposition[1], end=" ")
+    else:
+        print (Fore.BLUE + proposition[1], end=" ")
     
-if(proposition[4] == mot_mystere[4]):
-    print (Fore.RED + proposition[4], end=" ")
-elif(proposition[4] == mot_mystere[0] or proposition[4] == mot_mystere[1] or proposition[4] == mot_mystere[2] or proposition[4] == mot_mystere[3] or proposition[4] == mot_mystere[5]):
-    print (Fore.YELLOW + proposition[4], end=" ")
-else:
-    print (Fore.BLUE + proposition[4], end=" ")
+    if(proposition[2] == mot_mystere[2]):
+        print (Fore.RED + proposition[2], end=" ")
+    elif(proposition[2] == mot_mystere[0] or proposition[2] == mot_mystere[1] or proposition[2] == mot_mystere[3] or proposition[2] == mot_mystere[4] or proposition[2] == mot_mystere[5]):
+        print (Fore.YELLOW + proposition[2], end=" ")
+    else:
+        print (Fore.BLUE + proposition[2], end=" ")
+
+    if(proposition[3] == mot_mystere[3]):
+        print (Fore.RED + proposition[3], end=" ")
+    elif(proposition[3] == mot_mystere[0] or proposition[3] == mot_mystere[1] or proposition[3] == mot_mystere[2] or proposition[3] == mot_mystere[4] or proposition[3] == mot_mystere[5]):
+        print (Fore.YELLOW + proposition[3], end=" ")
+    else:
+        print (Fore.BLUE + proposition[3], end=" ")
     
-if(proposition[5] == mot_mystere[5]):
-    print (Fore.RED + proposition[5], end=" ")
-elif(proposition[5] == mot_mystere[0] or proposition[5] == mot_mystere[1] or proposition[5] == mot_mystere[2] or proposition[5] == mot_mystere[3] or proposition[5] == mot_mystere[4]):
-    print (Fore.YELLOW + proposition[5], end=" ")
-else:
-    print (Fore.BLUE + proposition[5], end=" ")
+    if(proposition[4] == mot_mystere[4]):
+        print (Fore.RED + proposition[4], end=" ")
+    elif(proposition[4] == mot_mystere[0] or proposition[4] == mot_mystere[1] or proposition[4] == mot_mystere[2] or proposition[4] == mot_mystere[3] or proposition[4] == mot_mystere[5]):
+        print (Fore.YELLOW + proposition[4], end=" ")
+    else:
+        print (Fore.BLUE + proposition[4], end=" ")
     
+    if(proposition[5] == mot_mystere[5]):
+        print (Fore.RED + proposition[5], end=" ")
+    elif(proposition[5] == mot_mystere[0] or proposition[5] == mot_mystere[1] or proposition[5] == mot_mystere[2] or proposition[5] == mot_mystere[3] or proposition[5] == mot_mystere[4]):
+        print (Fore.YELLOW + proposition[5], end=" ")
+    else:
+        print (Fore.BLUE + proposition[5], end=" ")
+        
+    if (proposition == mot_mystere):
+        victoire = True
+    tour += 1
+
 input ()
