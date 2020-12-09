8af9440dcc05f239eca3b258143e64487ab471af
Léo Masson <l.masson@rns.ecolescreatives.com>
2020-12-09 14:54:37 +0100

    Update Exam_Python_Leo_Masson_09_12.py

--- Exam_Python_Leo_Masson_09_12.py
+++ Exam_Python_Leo_Masson_09_12.py
@@ -5,3 +5,9 @@ from colorama import Fore, Back, Style
 import random
 
 liste_mot = ["hideux", "picole", "crayon", "fusain", "paumes", "charge", "tisser", "whisky", "chlore", "poulet"]
+
+mot_mystere = random.choices(liste_mot)
+print (liste_mot)
+print(mot_mystere)
+
+input ()
